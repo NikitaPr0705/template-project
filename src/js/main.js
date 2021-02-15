84717fe04d633e172bcfd72d04994fe42f7e68c7
NikitPr0705 <iamnik0705@gmail.com>
2021-02-15 21:45:13 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+/**
+  * название функции
+  *
+  * @param {number} first - первое число
+  * @returns {number}
+  */
+
+//  let closeBtn = document.querySelector('.close');
+
+//  closeBtn.addEventListener('click', function () {
+//      document.querySelector('.drop-down-menu.__header--mobile').classList.toggle('active');
+//      document.querySelector('.burger span').classList.toggle("active");
+//  })
+ 
+//  document.querySelector('.burger').addEventListener('click', function(){
+//      document.querySelector('.drop-down-menu.__header--mobile').classList.toggle('active');
+//      document.querySelector('.burger span').classList.toggle("active");
+//  });
+ 
+ 
+//  let dropDownBtnHeader = document.querySelector('.nav-list__item.__header--mobile.active');
+//  let dropDownInner = document.querySelector('.drop-down-inner.__header-inner.mobile');
+ 
+//  dropDownBtnHeader.addEventListener('click', function () {
+//      dropDownInner.classList.toggle('__header-inner__active');
+//      document.querySelector('.arrow-drop-down-header').classList.toggle('active-rotate');
+//  })
+ 
+ 
+ 
---
+++ src/js/vendor/slider-slick.js
@@ -0,0 +1,27 @@
+
+// $(document).ready(function () {
+//     $('.slider').slick({
+//         infinite: false,
+//         slidesToShow: 3,
+//         // variableWidth: false,
+//         // autoplay: true,
+//         responsive: [
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 1,
+//                 }
+//             },
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     slidesToShow: 2,
+//                 }
+//             },
+
+//         ],
+
+
+//     });
+
+// })
